2445bd64f5d1845433fe03643bead26d328d3686
Tham <zarub2k@gmail.com>
2022-06-07 23:35:56 +0530

    api card style added

--- src/components/ApiCard.vue
+++ src/components/ApiCard.vue
@@ -1,16 +1,25 @@
 <script setup>
+import {HeartIcon} from '@heroicons/vue/outline'
 const props = defineProps(['api'])
 </script>
 
 <template>
   <div class="apicard">
-    <h6 class="text-center">{{ api.name }}</h6>
-    <p class="text-sm">{{ api.summary }}</p>
+    <div class="flex">
+      <h6 class="text-center mb-2 grow">{{ api.name }}</h6>
+      <a href=""><HeartIcon class="icon"/></a>
+    </div>
+    <p class="text-sm grow">{{ api.summary }}</p>
+    <p class="mt-4"><button class="btn-secondary">View details</button></p>
   </div>
 </template>
 
 <style scoped>
 .apicard {
-  @apply border p-2
+  @apply flex flex-col border px-3 py-2
+}
+
+.icon {
+  @apply h-5 w-5 text-api-900
 }
 </style>
